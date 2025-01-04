3b5a7bcdbe6101cd3d6c3b57a679e7b1fc410c60
Junior Hirwa <iamhirwejr@gmail.com>
2025-01-04 13:40:00 +0200

    chore: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import LoginForm from "./pages/auth/LoginForm"
+import SignUpForm from "./pages/auth/SignupForm"
+// import SignUpForm from "./pages/auth/SignupForm"
+
 function App() {
   return (
-    <div className="text-3xl font-bold underline text-green-400">
-      Hello there
-    </div>
+   <>
+    <LoginForm/>
+    <SignUpForm/>
+   </>
   )
 }
 
