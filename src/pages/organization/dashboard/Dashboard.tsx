1cc1eb3014a407bd4e6f199aab9a04b0f09fb188
Junior Hirwa <iamhirwejr@gmail.com>
2025-01-21 20:25:28 +0200

    refactor: update  dashboard

--- src/pages/organization/dashboard/Dashboard.tsx
+++ src/pages/organization/dashboard/Dashboard.tsx
@@ -6,22 +6,23 @@ import Menu from "../../../components/ui/Menu/Menu";
 function Dashboard() {
   const { isLoading } = UserAuth();
 
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div className="h-screen grid grid-cols-[280px_1fr_1fr] grid-rows-[50px_1fr]">
-      {isLoading && (
-        <div className="flex items-center justify-center">
-          <Spinner />
-        </div>
-      )}
       <div className="col-start-1 col-end-1">
         <Sidebar />
       </div>
       <div className=" col-start-2 col-span-full row-span-1">
         <Menu />
       </div>
-      <div className="col-start-2 col-end-4 row-start-2">
-        dashboard content
-      </div>
+      <div className="col-start-2 col-end-4 row-start-2">dashboard content</div>
     </div>
   );
 }
